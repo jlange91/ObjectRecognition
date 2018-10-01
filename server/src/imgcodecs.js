321e5634321396e372e0ad441b9f2f641b7e8dec
lange julien <jlange@student.42.fr>
2018-10-01 18:40:20 +0200

    Remove useless data from base64

--- server/src/imgcodecs.js
+++ server/src/imgcodecs.js
@@ -1,6 +1,12 @@
 import cv from '/usr/lib/node_modules/opencv4nodejs';
 
-const decodeFromBase64 = base64Data => {
+const pngPrefix = 'data:image/jpeg;base64,';
+const jpgPrefix = 'data:image/png;base64,';
+
+const decodeFromBase64 = base64DataString => {
+  const base64Data = base64DataString
+    .replace(pngPrefix, '')
+    .replace(jpgPrefix, '');
   const buffer = Buffer.from(base64Data, 'base64');
   const img = cv.imdecode(buffer);
   return img;
