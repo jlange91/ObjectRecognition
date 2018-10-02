e87fe32743e75610e645178a8d0aa78657637222
lange julien <jlange@student.42.fr>
2018-10-02 19:43:38 +0200

    Modifying App style

--- client/app/containers/App/index.js
+++ client/app/containers/App/index.js
@@ -1,45 +1,14 @@
-/**
- *
- * App.js
- *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
- */
-
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import { withStyles } from '@material-ui/core/styles';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import { blue } from '@material-ui/core/colors';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 
 import HomePage from 'containers/HomePage';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import Header from 'components/Header';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#757ce8',
-      main: blue[300],
-      dark: '#002884',
-      contrastText: '#fff',
-    },
-  },
-});
-
-const styles = {
-  root: {
-    minHeight: '100%',
-    minWidth: '440px',
-    backgroundColor: '#F2F2F2',
-  },
-};
+import { styles, theme } from './styles';
 
 function App(props) {
   const { classes } = props;
@@ -48,10 +17,14 @@ function App(props) {
     <MuiThemeProvider theme={theme}>
       <div className={classes.root}>
         <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route component={NotFoundPage} />
-        </Switch>
+        <div className={classes.wrapper1}>
+          <div className={classes.wrapper2}>
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route component={NotFoundPage} />
+            </Switch>
+          </div>
+        </div>
       </div>
     </MuiThemeProvider>
   );
---
+++ client/app/containers/App/styles.js
@@ -0,0 +1,31 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { blue } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: blue[300],
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+  },
+});
+
+const styles = {
+  root: {
+    minHeight: '100%',
+    minWidth: '440px',
+    backgroundColor: '#F2F2F2',
+  },
+  wrapper1: {
+    padding: '10px',
+    overflow: 'scroll',
+  },
+  wrapper2: {
+    backgroundColor: '#EAEAEA',
+    padding: '10px',
+  },
+};
+
+export { theme, styles };
